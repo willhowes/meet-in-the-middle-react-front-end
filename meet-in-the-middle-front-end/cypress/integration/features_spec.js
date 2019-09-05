f51888d0ea7c3806453a06bf0ea12aa52d09ec86
Rianne McCartney <riannemccartney@hotmail.com>
2019-09-05 11:20:27 +0100

    Installed cypress and added new tests Co-authored-by: Genny <gennyallcroft@hotmail.co.uk> Co-authored-by: RichienoT <richieganney@gmail.com>

---
+++ meet-in-the-middle-front-end/cypress/integration/features_spec.js
@@ -0,0 +1,21 @@
+describe("Cypress", () => {
+  it("is working", () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe("visiting the home page", () => {
+  it("visits the home page", () => {
+    cy.visit("localhost:3000");
+  });
+
+  it("allows the user to enter an address", () => {
+    cy.visit("localhost:3000");
+    cy.get("address_text_box1")
+      .type("50 Commercial St, Spitalfields, London E1 6LT, UK")
+      .should(
+        "have.value",
+        "50 Commercial St, Spitalfields, London E1 6LT, UK"
+      );
+  });
+});
