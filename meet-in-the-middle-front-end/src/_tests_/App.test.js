699dd5bf83c630e9398ca0bc50f16a09755e3e18
richieganney <richieganney@gmail.com>
2019-09-05 15:47:17 +0100

    reorg of files

--- meet-in-the-middle-front-end/src/_tests_/App.test.js
+++ meet-in-the-middle-front-end/src/_tests_/App.test.js
@@ -9,10 +9,10 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('tests the middle point constructor is working', () => {
-  const middlePoint = new MiddlePoint();
-  expect(middlePoint.props)
-})
+// it('tests the middle point constructor is working', () => {
+//   const middlePoint = new MiddlePoint();
+//   expect(middlePoint.props)
+// })
 
 // it('returns the middle location', () => {
 //     const markers = [{
@@ -26,18 +26,3 @@ it('tests the middle point constructor is working', () => {
 //   const value = MidlLocation.midlLocation();
 //   expect(value).toBe(1)
 // });
-
-class MidlLocation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ""
-    };
-    this.midlLocation = this.midlLocation.bind(this);
-  }
-
-  midlLocation() {
-    if (this.props.markers[2]) {
-      return `lat: ${this.props.markers[2].position.lat}, lng: ${this.props.markers[2].position.lng}`;
-    }
-  }
