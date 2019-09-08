dbd8bff1127dedb381fd1edb832403359007c80c
richieganney <richieganney@gmail.com>
2019-09-08 17:27:07 +0100

    refactor

--- src/midlLocation/MidlLocation.js
+++ src/midlLocation/MidlLocation.js
@@ -5,9 +5,11 @@ class MidlLocation extends React.Component {
 
   render() {
     return (
+      <div style={{margin: 10}}>
         <div style={{ overflowY: "scroll", maxHeight:"250px" }}>
           <MidlPlaces places={this.props.places} />
         </div>
+      </div>
     );
   }
 }
