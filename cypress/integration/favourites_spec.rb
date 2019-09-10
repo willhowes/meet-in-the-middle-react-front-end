f7cb6d4c4a54bf12ad7c19a31c10112f4fe935f4
Rianne McCartney <riannemccartney@hotmail.com>
2019-09-10 14:24:08 +0100

    extra files deleted

--- cypress/integration/favourites_spec.rb
+++ cypress/integration/favourites_spec.rb
@@ -17,3 +17,17 @@ describe("User can save their home address as a favourite", () => {
       );
   });
 });
+
+describe("User can save their work address as a favourite", () => {
+
+  it("allows the user to enter an address", () => {
+    cy.visit("localhost:3000");
+    cy.get('input[id="profile_button"]').click({ force: true });
+    cy.get("#work_location_text_box")
+      .type("50 Commercial St, Spitalfields, London E1 6LT, UK")
+      .should(
+        'have.value',
+        "50 Commercial St, Spitalfields, London E1 6LT, UK"
+      );
+  });
+});
