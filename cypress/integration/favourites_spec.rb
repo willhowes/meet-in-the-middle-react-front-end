2263056391d58e61c9009f790d745aca17ad8f21
Rianne McCartney <riannemccartney@hotmail.com>
2019-09-10 13:49:18 +0100

    Cypress test for home location working Co-authored-by: Will <williameldenhowes@gmail.com>

--- cypress/integration/favourites_spec.rb
+++ cypress/integration/favourites_spec.rb
@@ -8,7 +8,7 @@ describe("User can save their home address as a favourite", () => {
 
   it("allows the user to enter an address", () => {
     cy.visit("localhost:3000");
-    cy.get('input[id="profile_button"]').click;
+    cy.get('input[id="profile_button"]').click({ force: true });
     cy.get("#home_location_text_box")
       .type("50 Commercial St, Spitalfields, London E1 6LT, UK")
       .should(
